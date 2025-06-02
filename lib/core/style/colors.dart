5ef0cceae7b4eb1c6501637597ff7f636da2fcf6
hamad <mo7maaa7maaa@gmail.com>
2025-06-02 12:24:27 +0300

    Colors updated

--- lib/core/style/colors.dart
+++ lib/core/style/colors.dart
@@ -3,6 +3,8 @@ import 'dart:ui';
 import 'package:flutter/material.dart';
 
 class AppColors {
+  static const Color black = Colors.black;
+  static const Color white = Colors.white;
   static const Color green = Colors.green;
   static const Color blue = Colors.blue;
   static const Color red = Colors.red;
